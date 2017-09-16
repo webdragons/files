fcfa7079ef27b62988f30273c4036c0c1db02ca9
Artem Kravchenko <lan143@lan143.ru>
2017-09-16 13:39:40 +0300

    Рефакторинг

--- src/models/File.php
+++ src/models/File.php
@@ -7,6 +7,7 @@ use bulldozer\files\enums\FileTypesEnum;
 use bulldozer\users\models\User;
 use yii\behaviors\BlameableBehavior;
 use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
 use yii\helpers\FileHelper;
 use yii\helpers\Inflector;
@@ -32,18 +33,18 @@ class File extends ActiveRecord
     /**
      * @inheritdoc
      */
-    public function behaviors()
+    public function behaviors() : array
     {
         return [
             [
-                'class' => TimestampBehavior::className(),
+                'class' => TimestampBehavior::class,
                 'attributes' => [
                     ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
                     ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
                 ],
             ],
             [
-                'class' => BlameableBehavior::className(),
+                'class' => BlameableBehavior::class,
                 'createdByAttribute' => 'creator_id',
                 'updatedByAttribute' => 'updater_id',
             ],
@@ -53,7 +54,7 @@ class File extends ActiveRecord
     /**
      * @inheritdoc
      */
-    public static function tableName()
+    public static function tableName() : string
     {
         return '{{%files}}';
     }
@@ -61,7 +62,7 @@ class File extends ActiveRecord
     /**
      * @inheritdoc
      */
-    public function rules()
+    public function rules() : array
     {
         return [
             [['created_at', 'updated_at', 'creator_id', 'updater_id', 'type'], 'integer'],
@@ -71,17 +72,17 @@ class File extends ActiveRecord
     }
 
     /**
-     * @return \yii\db\ActiveQuery
+     * @return ActiveQuery
      */
-    public function getCreator()
+    public function getCreator() : ActiveQuery
     {
         return $this->hasOne(User::className(), ['id' => 'creator_id']);
     }
 
     /**
-     * @return \yii\db\ActiveQuery
+     * @return ActiveQuery
      */
-    public function getUpdater()
+    public function getUpdater() : ActiveQuery
     {
         return $this->hasOne(User::className(), ['id' => 'updater_id']);
     }
@@ -89,7 +90,7 @@ class File extends ActiveRecord
     /**
      * @return string
      */
-    public function getFullUrl()
+    public function getFullUrl() : string
     {
         $url = urlencode($this->file_path);
         $url = str_replace('%2F', '/', $url);
@@ -102,27 +103,27 @@ class File extends ActiveRecord
      * @param UploadedFile $file
      * @return bool
      */
-    public function upload(UploadedFile $file)
+    public function upload(UploadedFile $file) : bool
     {
         if (strpos($file->type, "image") !== false) {
             $this->type = FileTypesEnum::TYPE_IMAGE;
-            $out_dir = '/uploads/images/'.substr(md5(time()), 0, 2).'/'.substr(md5(time()+1), 0, 2).'/';
+            $out_dir = '/images/'.substr(md5(time()), 0, 2).'/'.substr(md5(time()+1), 0, 2).'/';
         } else {
             $this->type = FileTypesEnum::TYPE_OTHER;
-            $out_dir = '/uploads/files/'.substr(md5(time()), 0, 2).'/'.substr(md5(time()+1), 0, 2).'/';
+            $out_dir = '/files/'.substr(md5(time()), 0, 2).'/'.substr(md5(time()+1), 0, 2).'/';
         }
 
         $out_file_name = Inflector::slug($file->baseName) . '.' . $file->extension;
         $out_original_file_name = substr(md5(time()), 0, 5) . '-' . md5(time() + 1) . '.' . $file->extension;
 
-        FileHelper::createDirectory(App::getAlias("@frontend") . '/web' . $out_dir);
+        FileHelper::createDirectory(App::getAlias("@uploads") . $out_dir);
 
-        if ($file->saveAs(App::getAlias("@frontend") . '/web' . $out_dir . $out_file_name)) {
+        if ($file->saveAs(App::getAlias("@uploads") . $out_dir . $out_file_name)) {
             $this->file_path = $out_dir . $out_file_name;
 
             if ($this->type == FileTypesEnum::TYPE_IMAGE) {
-                copy(App::getAlias("@frontend") . '/web' . $out_dir . $out_file_name,
-                    App::getAlias("@frontend") . '/web' . $out_dir . $out_original_file_name);
+                copy(App::getAlias("@uploads") . $out_dir . $out_file_name,
+                    App::getAlias("@uploads") . $out_dir . $out_original_file_name);
                 $this->origin_file_path = $out_dir . $out_original_file_name;
             } else {
                 $this->origin_file_path = $this->file_path;
@@ -141,8 +142,8 @@ class File extends ActiveRecord
     {
         parent::afterDelete();
 
-        $file_name = App::getAlias('@frontend') . '/web' . $this->file_path;
-        $origin_file_name = App::getAlias('@frontend') . '/web' . $this->origin_file_path;
+        $file_name = App::getAlias('@uploads') . $this->file_path;
+        $origin_file_name = App::getAlias('@uploads') . $this->origin_file_path;
 
         if (file_exists($file_name) && !is_dir($file_name)) {
             unlink($file_name);
@@ -151,21 +152,5 @@ class File extends ActiveRecord
         if (file_exists($origin_file_name) && !is_dir($origin_file_name)) {
             unlink($origin_file_name);
         }
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function attributeLabels()
-    {
-        return [
-            'id' => 'ID',
-            'created_at' => 'Created At',
-            'updated_at' => 'Updated At',
-            'creator_id' => 'Creator ID',
-            'updater_id' => 'Updater ID',
-            'type' => 'Type',
-            'file_path' => 'File Path',
-        ];
     }
 }
